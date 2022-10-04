b1ad2fd5071871d4e23d25ab5098cf649206237a
abxxbo <devavcado@gmail.com>
2022-10-04 18:47:31 -0500

    [shell] basic shell

---
+++ src/kernel/io/shell.asm
@@ -0,0 +1,73 @@
+%macro get_char 0
+	xor ax, ax
+	mov ah, 0x00
+	int 0x16
+%endmacro
+
+%macro printc 1
+	mov ah, 0x0e
+	mov al, %1
+	int 0x10
+%endmacro
+
+shell:
+	get_char
+	mov cl, al
+	cmp cl, 13
+	je .Enter
+
+	cmp cl, 08
+	je .Backspace
+	jne .Print
+	.Enter:
+		mov si, 0
+		printc `\r`
+		printc `\n`
+
+    call prompt
+		;; jump back
+		jmp shell
+	.Backspace:
+		dec si
+		mov byte[buffer+si], byte `\0`
+
+		;; get current cursor position
+		mov ah, 0x03
+		mov bh, 0
+		int 0x10
+
+		mov ch, dl
+		mov bl, dh
+		dec ch
+
+		cmp dl, 3
+		jl shell
+
+		;; move cursor
+		mov ah, 0x02
+		mov bh, 0
+		mov dl, ch
+		mov dh, bl
+		int 0x10
+
+		jmp shell
+	.Print:
+		printc cl
+		
+		;; add CL to buffer
+		mov byte [buffer+si], byte cl
+		inc si
+
+		;; Loop!
+		jmp shell
+
+
+prompt:
+  ;; write ps1
+  mov bx, ps1
+  call printf
+  ret
+
+
+buffer: times 512 db 0
+ps1: db `someOS-> `, 0
--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -1,10 +1,15 @@
 [org 0x7e00]
 
+call prompt
+jmp shell
+
 jmp $
 
 ;; Includes
 %include "disk/disk.asm"
 %include "disk/disk_ops.asm"
+
+%include "io/shell.asm"
 
 ;;; Output
 %include "output.asm"
