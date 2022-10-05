6995490ca1ccdfe3ec0b1e98fbebf58e5cd54ab5
abxxbo <devavcado@gmail.com>
2022-10-05 15:53:08 -0500

    hex printing

--- src/kernel/output.asm
+++ src/kernel/output.asm
@@ -9,3 +9,39 @@ printf:
 		jmp .Loop
 	.Exit:
   	ret
+
+printoasc:
+    ;hex to ascii-hex
+    add al, 30h
+    cmp al, 39h
+    jle printaback
+    add al, 7h ;if letter
+printaback:
+    ret
+
+printh:
+	;output ch as 2 digit hex number
+	mov al, ch
+	and al, 0xf ;clear upper nibble, to get second digit only
+	call printoasc
+	mov ah, al ;store
+	mov al, ch
+	shr al, 4h ;get upper nibble, to get first digit only
+	call printoasc
+	mov ch, ah ;store
+	;output two numbers
+	mov ah, 0xe
+	int 10h
+	mov al, ch
+	int 10h
+	ret
+
+print_newline:
+	mov ah, 0x0e
+	mov al, `\r`
+	int 0x10
+
+	mov ah, 0x0e
+	mov al, `\n`
+	int 0x10
+	ret
