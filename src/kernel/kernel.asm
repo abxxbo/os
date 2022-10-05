46f563ef4883d1fe21d653f7503e500a7500a72d
abxxbo <devavcado@gmail.com>
2022-10-05 13:05:15 -0500

    set video mode

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -8,14 +8,21 @@ mov sp, 0x7e00
 mov bp, sp
 sti
 
+;; Set video mode
+mov ah, 0x00
+mov al, 0x03
+int 0x10
 
+;; Initialize drivers
+;; ...
+
+
+;; BEGIN ACTUAL CODE EXEC
 call get_ps1
 call prompt
 
 call printf
 jmp shell
-
-jmp $
 
 ;; Includes
 %include "disk/disk.asm"
