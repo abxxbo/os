7cca701948ba38f223a4687f58a60416b66b4e79
abxxbo <devavcado@gmail.com>
2022-10-05 22:44:22 -0500

    begin work on syscalls for a COM implementation

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -12,11 +12,19 @@ sti
 
 
 PIT_IRQ_OFFSET equ 8*4
+SYSCALL_OFFSET equ 0x80*0x04    ;; i think 0x200
+                                ;; i will let the compiler decide
 cli
 
 
 mov word [PIT_IRQ_OFFSET], _irq0_isr
 mov word [PIT_IRQ_OFFSET+2], ax
+
+
+;; Interrupt for syscalls.
+;; We will use int 0x80 (just like linux)
+mov word [SYSCALL_OFFSET], _syscall_hdlr
+mov word [SYSCALL_OFFSET+2], ax
 
 sti
 
@@ -40,6 +48,8 @@ jmp shell
 %include "io/shell.asm"
 
 %include "arch/pit.asm"
+%include "arch/syscalls.asm"
+
 
 %include "datetime.asm"
 
